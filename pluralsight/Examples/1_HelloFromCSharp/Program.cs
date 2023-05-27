782a5effdf2d332bc78124a24ce3ad2e2fdac81f
Md. Jabed Hossan <jabed.cse.21@gmail.com>
2023-05-27 23:13:07 +0600

    Demo- Writing c# code

---
+++ pluralsight/Examples/1_HelloFromCSharp/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _1_HelloFromCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            Console.WriteLine("Please enter your name: ");
+
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name);
+        }
+    }
+}
