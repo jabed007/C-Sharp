6a40ac524921d82deca1f052c776947271e0287a
Md. Jabed Hossan <jabed.cse.21@gmail.com>
2023-05-29 14:47:22 +0600

    Building blocks c# demo

---
+++ pluralsight/Examples/BethanysPieShopHRM/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace BethanysPieShopHRM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Bethany's Pie Shop HRM");
+
+            Console.WriteLine("Please enter your name: ");
+
+            string name = Console.ReadLine();
+
+            string name2 = Console.ReadLine();
+
+            string name_2 = Console.ReadLine();
+
+            string Name_2 = Console.ReadLine();
+        }
+    }
+}
