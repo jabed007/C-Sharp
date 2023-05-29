0fbb161eb83f143008a3de2eb2ec79e2eef22b83
Md. Jabed Hossan <jabed.cse.21@gmail.com>
2023-05-29 16:12:00 +0600

    Demo-Creating C# String

--- pluralsight/Examples/BethanysPieShopHRM/Program.cs
+++ pluralsight/Examples/BethanysPieShopHRM/Program.cs
@@ -8,13 +8,42 @@
 
             Console.WriteLine("Please enter your name: ");
 
+            //string name = Console.ReadLine();
+
+            //string name2 = Console.ReadLine();
+
+            //string name_2 = Console.ReadLine();
+
+            //string Name_2 = Console.ReadLine();
+
+            int monthlyWage = 1234;
+
+            int months = 12, bonus = 1000;
+
+            bool isActive = true;
+
+            double rating = 99.25;
+
+            //byte numberOfEmployees = 300;
+
+            int hoursWorked = 125;
+
+
+            hoursWorked = 148;
+
+            //monthlyWage = true;
+            const double interestRate = 0.07;
+
+            //interestRate = 0.08;
+
+            string firstName = "Bethany";
+            string lastName = "Smith";
+
+            string emptyString = "";
+
+            Console.WriteLine("Please enter your name: ");
             string name = Console.ReadLine();
 
-            string name2 = Console.ReadLine();
-
-            string name_2 = Console.ReadLine();
-
-            string Name_2 = Console.ReadLine();
         }
     }
 }
