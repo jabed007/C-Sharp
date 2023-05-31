7a7c65640da762d884d22b38ff4494ed2f1bc005
Md. Jabed Hossan <jabed.cse.21@gmail.com>
2023-05-31 17:04:43 +0600

    switching statemenh

--- pluralsight/Examples/BethanysPieShopHRM/Program.cs
+++ pluralsight/Examples/BethanysPieShopHRM/Program.cs
@@ -131,6 +131,29 @@
             {
                 Console.WriteLine("Payment will be late");
             }
+
+            Console.WriteLine("Choose the action you want to do: ");
+            Console.WriteLine("1. Add employee");
+            Console.WriteLine("2. Update employee");
+            Console.WriteLine("3. Delete employee");
+
+            string selectedAction = Console.ReadLine();
+
+            switch(selectedAction)
+            {
+                case "1":
+                    Console.WriteLine("Adding new employee ...");
+                    break;
+                case "2":
+                    Console.WriteLine("Updating employee ...");
+                    break;
+                case "3":
+                    Console.WriteLine("Deleting employee ...");
+                    break;
+                default: 
+                    Console.WriteLine("Invalid Input");
+                    break;
+            }
         }
     }
 }
