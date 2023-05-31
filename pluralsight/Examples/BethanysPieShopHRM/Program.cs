9c882cd0eb5ceb198d3bf5ecb82ee29afa8f38ed
Md. Jabed Hossan <jabed.cse.21@gmail.com>
2023-05-31 09:49:44 +0600

    working with date time

--- pluralsight/Examples/BethanysPieShopHRM/Program.cs
+++ pluralsight/Examples/BethanysPieShopHRM/Program.cs
@@ -4,9 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Bethany's Pie Shop HRM");
+            //Console.WriteLine("Welcome to Bethany's Pie Shop HRM");
 
-            Console.WriteLine("Please enter your name: ");
+            //Console.WriteLine("Please enter your name: ");
 
             //string name = Console.ReadLine();
 
@@ -16,65 +16,101 @@
 
             //string Name_2 = Console.ReadLine();
 
-            int monthlyWage = 1234;
+            //int monthlyWage = 1234;
 
-            int months = 12, bonus = 1000;
+            //int months = 12, bonus = 1000;
 
-            bool isActive = true;
+            //bool isActive = true;
 
-            double rating = 99.25;
+            //double rating = 99.25;
 
-            //byte numberOfEmployees = 300;
+            ////byte numberOfEmployees = 300;
 
-            int hoursWorked = 125;
+            //int hoursWorked = 125;
 
 
-            hoursWorked = 148;
+            //hoursWorked = 148;
 
-            //monthlyWage = true;
-            const double interestRate = 0.07;
+            ////monthlyWage = true;
+            //const double interestRate = 0.07;
 
-            //interestRate = 0.08;
+            ////interestRate = 0.08;
 
-            string firstName = "Bethany";
-            string lastName = "Smith";
+            //string firstName = "Bethany";
+            //string lastName = "Smith";
 
-            string emptyString = "";
+            //string emptyString = "";
 
-            Console.WriteLine("Please enter your name: ");
-            string name = Console.ReadLine();
+            //Console.WriteLine("Please enter your name: ");
+            //string name = Console.ReadLine();
 
-            double ratePerHour = 12.34;
-            int numberOfHoursWorked = 165;
+            //double ratePerHour = 12.34;
+            //int numberOfHoursWorked = 165;
 
-            double currentMonthWage = ratePerHour * numberOfHoursWorked + bonus;
-            Console.WriteLine(currentMonthWage);
+            //double currentMonthWage = ratePerHour * numberOfHoursWorked + bonus;
+            //Console.WriteLine(currentMonthWage);
 
-            ratePerHour += 3;
-            Console.WriteLine(ratePerHour);
+            //ratePerHour += 3;
+            //Console.WriteLine(ratePerHour);
 
-            if (currentMonthWage > 2000)
-                Console.WriteLine("Top paid employee!");
+            //if (currentMonthWage > 2000)
+            //    Console.WriteLine("Top paid employee!");
 
-            int numberOfEmployees = 15;
-            numberOfEmployees--;
-            Console.WriteLine(numberOfEmployees);
+            //int numberOfEmployees = 15;
+            //numberOfEmployees--;
+            //Console.WriteLine(numberOfEmployees);
 
-            bool a = true;
-            int b = 10;
+            //bool a = true;
+            //int b = 10;
 
-            Console.WriteLine(a);
+            //Console.WriteLine(a);
 
-            int intMaxValue = int.MaxValue;
-            int intMinValue = int.MinValue;
+            //int intMaxValue = int.MaxValue;
+            //int intMinValue = int.MinValue;
 
-            char userSelction = 'a';
-            char upperVersion = char.ToUpper(userSelction);
+            //char userSelction = 'a';
+            //char upperVersion = char.ToUpper(userSelction);
 
-            bool isDigit = char.IsDigit(userSelction);
-            bool isLetter = char.IsLetter(userSelction);
+            //bool isDigit = char.IsDigit(userSelction);
+            //bool isLetter = char.IsLetter(userSelction);
 
-            Console.ReadLine();
+            //Console.ReadLine();
+
+            //DateTime hireDate = new DateTime(2023, 3, 23, 14, 30, 0);
+            //Console.WriteLine(hireDate);
+
+            //DateTime exitDate = new DateTime(2025, 8, 12);
+            //Console.WriteLine(exitDate);
+
+            ////DateTime invalidDate = new DateTime(2022, 13, 32); // unrepresentable date time
+            ////Console.WriteLine(invalidDate);
+
+            //DateTime startDate = hireDate.AddDays(15);
+            //Console.WriteLine(startDate);
+
+            //DateTime currentDate = DateTime.Now;
+            //Console.WriteLine(currentDate);
+
+            //bool areWeInDst = currentDate.IsDaylightSavingTime();
+
+
+            DateTime startHour = DateTime.Now;
+            TimeSpan workHour = new TimeSpan(8, 35, 00);
+            DateTime endHour = startHour.Add(workHour);
+
+            Console.WriteLine(startHour);
+            Console.WriteLine(workHour);
+            Console.WriteLine(endHour);
+
+            Console.WriteLine("---------------------------");
+
+            Console.WriteLine(startHour.ToLongDateString());
+            Console.WriteLine(startHour.ToLongTimeString());
+
+            Console.WriteLine("---------------------------");
+
+            Console.WriteLine(endHour.ToShortDateString());
+            Console.WriteLine(endHour.ToShortTimeString());
 
         }
     }
