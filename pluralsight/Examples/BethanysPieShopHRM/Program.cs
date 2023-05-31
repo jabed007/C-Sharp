f54cd84a4aa644cb970b1f88f91368a2f340e139
Md. Jabed Hossan <jabed.cse.21@gmail.com>
2023-05-31 22:10:13 +0600

    add a helper file

--- pluralsight/Examples/BethanysPieShopHRM/Program.cs
+++ pluralsight/Examples/BethanysPieShopHRM/Program.cs
@@ -167,7 +167,7 @@
             //    selectedAction = Console.ReadLine();
             //}
 
-            string selectedAction;
+            //string selectedAction;
 
             //do
             //{
@@ -199,22 +199,9 @@
             int amount = 1234;
             int months = 12;
 
-            int yearlyWage = CalculateYearlyWage(amount, months);
+            int yearlyWage = Utilities.CalculateYearlyWage(amount, months);
 
             Console.WriteLine($"Yearly wage: {yearlyWage}");
-
-            static int CalculateYearlyWage(int monthlyWage, int numberOfMonthsWorked)
-            {
-                Console.WriteLine($"Yearly wage: {monthlyWage * numberOfMonthsWorked}");
-
-                // lets add a bonus month
-                if (numberOfMonthsWorked == 12)
-                {
-                    return monthlyWage * (numberOfMonthsWorked + 1);
-                }
-
-                return monthlyWage * numberOfMonthsWorked;
-            }
         }
     }
 }
