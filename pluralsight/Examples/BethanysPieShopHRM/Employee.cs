4eba727dc68fa0f930f09c17dfef575669cd96e2
Md. Jabed Hossan <jabed.cse.21@gmail.com>
2023-06-02 18:08:31 +0600

    Creating the Employee class

---
+++ pluralsight/Examples/BethanysPieShopHRM/Employee.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BethanysPieShopHRM
+{
+    internal class Employee
+    {
+        public string firstName;
+        public string lastName;
+        public string email;
+
+        public int numberOfHoursWorked;
+        public double wage;
+        public double hourlyRate;
+
+        public DateTime birthDay;
+
+        const int minimulHoursWorkedUnit = 1;
+
+        public void PerformWork()
+        {
+            PerformWork(minimulHoursWorkedUnit);
+        }
+
+        public void PerformWork(int numberOfHours)
+        {
+            numberOfHoursWorked += numberOfHours;
+            Console.WriteLine($"The {firstName} {lastName} has worked for {numberOfHoursWorked} hour(s)!");
+        }
+
+        public double ReceiveWage(bool resetHours = true)
+        {
+            wage = numberOfHoursWorked * hourlyRate;
+
+            Console.WriteLine($"{firstName} {lastName} has received a wage of {wage} for {numberOfHoursWorked} hour(s) of work");
+
+            if (resetHours )
+            {
+                numberOfHoursWorked = 0;
+            }
+
+            return wage;
+        }
+
+        public void EmployeeDetails()
+        {
+            Console.WriteLine($"\nFirst name: \t{firstName}\nLast name: \t{lastName}\nEmail: \t\t{email}\nBirth date: \t{birthDay.ToShortDateString()}");
+        }
+    }
+}
